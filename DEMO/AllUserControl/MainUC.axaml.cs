76a3c1ed3f5eed0ec8e0d795f48625e899504270
sofronovkola <sofronovkola2@gmail.com>
2026-03-23 15:09:47 +0900

    Обновил проект

--- DEMO/AllUserControl/MainUC.axaml.cs
+++ DEMO/AllUserControl/MainUC.axaml.cs
@@ -13,6 +13,7 @@ namespace DEMO.AllUserControl;
 public partial class MainUC : UserControl
 {
     public List<Product> ProductList{get; set;} = new List<Product>();
+    public List<Manufacturer> manufacturer1 { get; set; } = new List<Manufacturer>();
     public MainUC()
     {
         InitializeComponent();
@@ -20,12 +21,27 @@ public partial class MainUC : UserControl
                                       .Include(c => c.NameProductNavigation)
                                       .Include(c => c.Ordersproducts)
                                       .Include(c => c.SupplierNavigation).ToList();
-        //MUControl.Loaded += Control_OnLoaded; // Подписка на событие
-        //ProdustListB.SelectionChanged+=ProductListBox_OnSelectionChanged;
+
+        manufacturer1=Context.Connect.Manufacturers.ToList();
+        EventUI();
+    }
+
+    private void EventUI()
+    {
+        MUControl.Loaded += Control_OnLoaded; // Подписка на событие
+        ProdustListB.SelectionChanged+=ProductListBox_OnSelectionChanged;
         RemoveButton.Click += DeleteButton_OnClick;
         AddButton.Click+=AddButton_OnClick;
         EditButton.Click+=EditButton_OnClick;
+
+        //Подписки для реализации сортировки и поиска
+        ManufacturersCBox.SelectionChanged += ManufacturersCBox_OnSelectionChanged;
+
+        SearchTextB.TextChanged += SearchTextB_OnTextChanged;
+        SortUpButton.IsCheckedChanged += SortButton_OnChecked;
+        SortDownButton.IsCheckedChanged += SortButton_OnChecked;
     }
+
 
     public void Control_OnLoaded(object? sender, RoutedEventArgs e)
     {
@@ -60,5 +76,53 @@ public partial class MainUC : UserControl
     }
 
     App.MainWindow.MainContentControl.Content = new MainUC();
+    }
+
+    private string manufacturPar;
+    private string searchPar;
+    private int sort = 0;
+
+    void Search(string manufacturer, string search, int sort = -1)
+    {
+    ProductList = Context.Connect.Products.Include(c => c.ManufacturerNavigation).Where
+        (c => c.ManufacturerNavigation.Manufacturer1.Contains(manufacturPar) && c.NameProductNavigation.NameProduct.Contains(searchPar)).ToList();
+
+    if (sort == 1)
+    {
+        ProductList = ProductList.OrderBy(c => c.CountInStock).ToList();
+    }
+
+    if (sort == 0)
+    {
+        ProductList = ProductList.OrderByDescending(c => c.CountInStock).ToList();
+    }
+
+    ProdustListB.ItemsSource = ProductList;
+    }
+
+    private void ManufacturersCBox_OnSelectionChanged(object? sender, RoutedEventArgs e)
+    {
+    manufacturPar = ((Manufacturer)ManufacturersCBox.SelectedItem).Manufacturer1;
+    Search(manufacturPar, searchPar);
+    }
+
+    private void SearchTextB_OnTextChanged(object? sender, RoutedEventArgs e)
+    {
+    searchPar = SearchTextB.Text;
+    Search(manufacturPar, searchPar);
+    }
+
+    private void SortButton_OnChecked(object? sender, RoutedEventArgs e)
+    {
+    if (SortUpButton.IsChecked == true)
+    {
+        sort = 1;
+    }
+
+    if (SortDownButton.IsChecked == true)
+    {
+        sort = 0;
+    }
+    Search(manufacturPar, searchPar, sort);
     }
 }
--- DEMO/obj/Debug/net10.0/DEMO.AssemblyInfo.cs
+++ DEMO/obj/Debug/net10.0/DEMO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3feea02223e786bbf9a7fc43d1a0abfa1de6e23d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DEMO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
